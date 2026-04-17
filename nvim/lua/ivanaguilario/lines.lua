469c4b223fb09f2462f4334c1a026863d3254030
Ivan Aguilar <ivan@ivanaguilar.com>
2026-04-18 01:04:06 +0200

    chore(nvim): numbers in netrw

--- nvim/lua/ivanaguilario/lines.lua
+++ nvim/lua/ivanaguilario/lines.lua
@@ -1,2 +1,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function()
+    vim.opt_local.number = true
+    vim.opt_local.relativenumber = true
+  end,
+})
